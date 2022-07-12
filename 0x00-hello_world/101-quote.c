3de728a4c30183f0263d900e3bd36c8d8b7d1b0e
Ufuoma Goodluck Edhebru <ufuomaedhebru@gmail.com>
2022-07-11 20:48:20 -0700

    Hello World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+/*
+ * File: 101-quote.c
+ * Author: Brennan D Baraban
+ */
+
+#include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	write2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		59);
+
+	return (1);
+}
