7e179b9149aebd85a3fbe9f209fae762b6d57553
Ufuoma Goodluck Edhebru <ufuomaedhebru@gmail.com>
2022-07-14 21:05:44 -0700

    i tire

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAINH_H
+
+/*Skyiline shit*/
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -12,3 +17,5 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int main(void);
+
+#endif
